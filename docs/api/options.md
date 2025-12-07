8706da2338ea103b6337c501ff8c375bf1810ab7
Louis Maddox <louismmx@gmail.com>
2025-12-07 15:45:53 +0000

    fix: avoid showing Config twice

--- docs/api/options.md
+++ docs/api/options.md
@@ -1,3 +1,5 @@
 # options
 
 ::: page_dewarp.options
+    options:
+        members: ["core", "k_opt"]
--- src/page_dewarp/options/__init__.py
+++ src/page_dewarp/options/__init__.py
@@ -1,6 +1,7 @@
 """Option-handling and configuration definitions for page_dewarp.
 
 This package provides:
+
 - A global config instance (`cfg`) containing default parameters.
 - The `Config` class, which defines the structure and types of these parameters.
 """
