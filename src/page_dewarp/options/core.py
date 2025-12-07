d0ce055af36e07f2fef8cffe86d9019a8c8f264e
Louis Maddox <louismmx@gmail.com>
2025-12-07 17:49:57 +0000

    docs: document the CLI params in more detail

--- src/page_dewarp/options/core.py
+++ src/page_dewarp/options/core.py
@@ -31,7 +31,6 @@ class Config(Struct):
     output size, page margin, debug verbosity, etc.
 
     Attributes:
-        OPT_MAX_ITER (int): Maximum Powell's method optimisation iterations.
         FOCAL_LENGTH (float): Normalized focal length of camera.
         TEXT_MIN_WIDTH (int): Minimum reduced pixel width of detected text contour.
         TEXT_MIN_HEIGHT (int): Minimum reduced pixel height of detected text contour.
@@ -62,16 +61,32 @@ class Config(Struct):
     """
 
     OPT_MAX_ITER: desc(int, "Maximum Powell's method optimisation iterations") = 600_000
+    """
+    Maximum Powell's method optimisation iterations.
+
+    Note:
+        For a fast optimisation (to see a quick 'draft'), set it to a low value like 1.
+
+    This value is passed as `maxiter` to
+    [scipy.optimize.minimize](https://docs.scipy.org/doc/scipy/reference/optimize.minimize-powell.html),
+    which defaults to `N*1000` where N is the number of parameter variables (in our
+    case, 600).
+    """
     # [camera_opts]
     FOCAL_LENGTH: desc(float, "Normalized focal length of camera") = 1.2
+    """Normalized focal length of camera."""
     # [contour_opts]
     TEXT_MIN_WIDTH: desc(int, "Min reduced px width of detected text contour") = 15
+    """Min reduced px width of detected text contour."""
     TEXT_MIN_HEIGHT: desc(int, "Min reduced px height of detected text contour") = 2
+    """Min reduced px height of detected text contour."""
     TEXT_MIN_ASPECT: desc(float, "Filter out text contours below this w/h ratio") = 1.5
+    """Filter out text contours below this w/h ratio."""
     TEXT_MAX_THICKNESS: desc(
         int,
         "Max reduced px thickness of detected text contour",
     ) = 10
+    """Max reduced px thickness of detected text contour."""
     # [debug_lvl_opt]
     DEBUG_LEVEL: int = 0
     # [debug_out_opt]
@@ -81,46 +96,74 @@ class Config(Struct):
         float,
         "Max reduced px horiz. overlap of contours in span",
     ) = 1.0
+    """Max reduced px horiz. overlap of contours in span."""
     EDGE_MAX_LENGTH: desc(
         float,
         "Max reduced px length of edge connecting contours",
     ) = 100.0
+    """Max reduced px length of edge connecting contours."""
     EDGE_ANGLE_COST: desc(float, "Cost of angles in edges (tradeoff vs. length)") = 10.0
+    """Cost of angles in edges (tradeoff vs. length)."""
     EDGE_MAX_ANGLE: desc(float, "Maximum change in angle allowed between contours") = (
         7.5
     )
+    """Maximum change in angle allowed between contours."""
     # [image_opts]
     SCREEN_MAX_W: desc(int, "Viewing screen max width (for resizing to screen)") = 1280
+    """Viewing screen max width (for resizing to screen)."""
     SCREEN_MAX_H: desc(int, "Viewing screen max height (for resizing to screen)") = 700
+    """Viewing screen max height (for resizing to screen)."""
     PAGE_MARGIN_X: desc(int, "Reduced px to ignore near L/R edge") = 50
+    """Reduced px to ignore near L/R edge."""
     PAGE_MARGIN_Y: desc(int, "Reduced px to ignore near T/B edge") = 20
+    """Reduced px to ignore near T/B edge."""
     # [mask_opts]
     ADAPTIVE_WINSZ: desc(int, "Window size for adaptive threshold in reduced px") = 55
+    """Window size for adaptive threshold in reduced px."""
     # [output_opts]
     OUTPUT_ZOOM: desc(float, "How much to zoom output relative to *original* image") = (
         1.0
     )
+    """How much to zoom output relative to *original* image.
+
+    Note:
+        This controls output resolution, so 2.0 roughly (not exactly) doubles the
+        size. For example in [#19](https://github.com/lmmx/page-dewarp/issues/19):
+
+        - `1` => 800 x 1248 px (default)
+        - `2` => 1568 x 2480 px
+        - `3` => 2352 x 3712 px
+    """
     OUTPUT_DPI: desc(int, "Just affects stated DPI of PNG, not appearance") = 300
+    """Just affects stated DPI of PNG, not appearance."""
     REMAP_DECIMATE: desc(int, "Downscaling factor for remapping image") = 16
+    """Downscaling factor for remapping image."""
     NO_BINARY: desc(int, "Disable output conversion to binary thresholded image") = 0
+    """Disable output conversion to binary thresholded image."""
     # [pdf_opts]
     CONVERT_TO_PDF: desc(bool, "Merge dewarped images into a PDF") = False
+    """Merge dewarped images into a PDF."""
     # [proj_opts]
     RVEC_IDX: desc(
         tuple[int, int],
         "Index of rvec in params vector (slice: pair of values)",
     ) = (0, 3)
+    """Index of rvec in params vector (slice: pair of values)."""
     TVEC_IDX: desc(
         tuple[int, int],
         "Index of tvec in params vector (slice: pair of values)",
     ) = (3, 6)
+    """Index of tvec in params vector (slice: pair of values)."""
     CUBIC_IDX: desc(
         tuple[int, int],
         "Index of cubic slopes in params vector (slice: pair of values)",
     ) = (6, 8)
+    """Index of cubic slopes in params vector (slice: pair of values)."""
     # [span_opts]
     SPAN_MIN_WIDTH: desc(int, "Minimum reduced px width for span") = 30
+    """Minimum reduced px width for span."""
     SPAN_PX_PER_STEP: desc(int, "Reduced px spacing for sampling along spans") = 20
+    """Reduced px spacing for sampling along spans."""
 
 
 cfg = Config()
